00d2a8563837f5272af25c94c99419725fca5209
Natii09 <nataliafernandezperez09@gmail.com>
2026-04-10 10:51:50 -0500

    Prueba

--- functions/db2_queries.py
+++ functions/db2_queries.py
@@ -483,7 +483,7 @@ def get_db2_connection_health():
         stmt = ibm_db.exec_immediate(conn, sql)
         if stmt:
             result = ibm_db.fetch_assoc(stmt)
-            for key, value in result.items():                 
+            for key, value in result.items():            
                 if isinstance(value, datetime):                     
                     result[key] = value.isoformat()
             return {
